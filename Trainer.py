965d7243325179d944708021da15d2c3300b320d
stanleyshen2003 <stanley.shen2003@gmail.com>
2024-04-21 20:45:38 +0800

    trainer1

--- Trainer.py
+++ Trainer.py
@@ -3,7 +3,6 @@ import argparse
 import numpy as np
 import torch
 import torch.nn as nn
-from torchvision import transforms
 from torch.utils.data import DataLoader
 
 from dataloader import Dataset_Game
@@ -17,15 +16,7 @@ from tqdm import tqdm
 import imageio
 
 import matplotlib.pyplot as plt
-from math import log10
 
-def Generate_PSNR(imgs1, imgs2, data_range=1.):
-    """PSNR for torch tensor"""
-    mse = nn.functional.mse_loss(imgs1, imgs2) # wrong computation for batch size > 1
-    psnr = 20 * log10(data_range) - 10 * torch.log10(mse)
-    return psnr
-
-     
 
 class Cla_model(nn.Module):
     def __init__(self, args):
@@ -42,6 +33,7 @@ class Cla_model(nn.Module):
         self.current_epoch = 0
         
         self.batch_size = args.batch_size
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         
     def training_stage(self):
         for i in range(self.args.num_epoch):
@@ -49,8 +41,8 @@ class Cla_model(nn.Module):
             print(len(train_loader))
             total_loss = 0
             for (img, label) in (pbar := tqdm(train_loader, ncols=120)):
-                img = img.to(self.args.device)
-                label = label.to(self.args.device)
+                img = img.to(self.device)
+                label = label.to(self.device)
                 # training one step
                 self.optim.zero_grad()
                 result = self.resnet(img)
@@ -79,27 +71,16 @@ class Cla_model(nn.Module):
     @torch.no_grad()
     def eval(self):
         val_loader = self.val_dataloader()
+        total_loss = 0
         for (img, label) in (pbar := tqdm(val_loader, ncols=120)):
-            img = img.to(self.args.device)
-            label = label.to(self.args.device)
-            images, loss = self.val_one_step(img, label)
+            img = img.to(self.device)
+            label = label.to(self.device)
+            result = self.resnet(img)
+            loss = self.ce_criterion(result, label)
+            total_loss += loss.item()
             self.tqdm_bar('val', pbar, loss.detach().cpu(), lr=self.scheduler.get_last_lr()[0])
-            self.make_gif(images, os.path.join(self.args.save_root, f"epoch={self.current_epoch}.gif"))
-
-            
-    
-    def val_one_step(self, img, label):
-        loss = 0
-        images = [img[:, 0]]
-        with torch.no_grad():
-            last = img[:, 0]
-            for i in range(1, self.val_vi_len):
-                out, _, _ = self.forward(last, img[:, i], label[:, i])
-                images.append(out)
-                last = out.detach()
-                loss += Generate_PSNR(out, img[:, i])
-
-        return images, loss
+        print(f"Epoch {self.current_epoch}, val_loss: {total_loss/len(val_loader)}")
+        
     def train_dataloader(self):
         dataset = Dataset_Game(root=self.args.DR, mode='train')
             
@@ -127,7 +108,6 @@ class Cla_model(nn.Module):
             "state_dict": self.state_dict(),
             "optimizer": self.state_dict(),  
             "lr"        : self.scheduler.get_last_lr()[0],
-            "tfr"       :   self.tfr,
             "last_epoch": self.current_epoch
         }, path)
         print(f"save ckpt to {path}")
@@ -141,12 +121,8 @@ class Cla_model(nn.Module):
             
             self.optim      = optim.Adam(self.parameters(), lr=self.args.lr)
             self.scheduler  = optim.lr_scheduler.MultiStepLR(self.optim, milestones=[2, 4], gamma=0.1)
-            self.kl_annealing = kl_annealing(self.args, current_epoch=checkpoint['last_epoch'])
             self.current_epoch = checkpoint['last_epoch']
 
-    def optimizer_step(self):
-        nn.utils.clip_grad_norm_(self.parameters(), 1.)
-        self.optim.step()
 
 
 
@@ -167,9 +143,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument('--batch_size',    type=int,    default=2)
     parser.add_argument('--lr',            type=float,  default=0.001,     help="initial learning rate")
-    parser.add_argument('--device',        type=str, choices=["cuda", "cpu"], default="cuda")
-    parser.add_argument('--optim',         type=str, choices=["Adam", "AdamW"], default="Adam")
-    parser.add_argument('--gpu',           type=int, default=1)
     parser.add_argument('--test',          action='store_true')
     parser.add_argument('--store_visualization',      action='store_true', help="If you want to see the result while training")
     parser.add_argument('--DR',            type=str, required=True,  help="Your Dataset Path")
@@ -177,34 +150,11 @@ if __name__ == '__main__':
     parser.add_argument('--num_workers',   type=int, default=4)
     parser.add_argument('--num_epoch',     type=int, default=70,     help="number of total epoch")
     parser.add_argument('--per_save',      type=int, default=3,      help="Save checkpoint every seted epoch")
-    parser.add_argument('--partial',       type=float, default=1.0,  help="Part of the training dataset to be trained")
-    parser.add_argument('--train_vi_len',  type=int, default=16,     help="Training video length")
-    parser.add_argument('--val_vi_len',    type=int, default=630,    help="valdation video length")
     parser.add_argument('--frame_H',       type=int, default=32,     help="Height input image to be resize")
     parser.add_argument('--frame_W',       type=int, default=64,     help="Width input image to be resize")
     
     
-    # Module parameters setting
-    parser.add_argument('--F_dim',         type=int, default=128,    help="Dimension of feature human frame")
-    parser.add_argument('--L_dim',         type=int, default=32,     help="Dimension of feature label frame")
-    parser.add_argument('--N_dim',         type=int, default=12,     help="Dimension of the Noise")
-    parser.add_argument('--D_out_dim',     type=int, default=192,    help="Dimension of the output in Decoder_Fusion")
-    
-    # Teacher Forcing strategy
-    parser.add_argument('--tfr',           type=float, default=1.0,  help="The initial teacher forcing ratio")
-    parser.add_argument('--tfr_sde',       type=int,   default=10,   help="The epoch that teacher forcing ratio start to decay")
-    parser.add_argument('--tfr_d_step',    type=float, default=0.05,  help="Decay step that teacher forcing ratio adopted")
     parser.add_argument('--ckpt_path',     type=str,    default=None,help="The path of your checkpoints")   
-    
-    # Training Strategy
-    parser.add_argument('--fast_train',         action='store_true')
-    parser.add_argument('--fast_partial',       type=float, default=0.4,    help="Use part of the training data to fasten the convergence")
-    parser.add_argument('--fast_train_epoch',   type=int, default=5,        help="Number of epoch to use fast train mode")
-    
-    # Kl annealing stratedy arguments
-    parser.add_argument('--kl_anneal_type',     type=str, default='Cyclical',       help="")
-    parser.add_argument('--kl_anneal_cycle',    type=int, default=10,               help="")
-    parser.add_argument('--kl_anneal_ratio',    type=float, default=1,              help="")
     
 
     
